d581d427374197ac62be67e4c191452fe64cfb8f
Nicorb72 <141965721+Nicorb72@users.noreply.github.com>
2023-10-03 07:13:04 -0500

    1

---
+++ src/main/java/vehiculos/Pais.java
@@ -0,0 +1,40 @@
+package vehiculos;
+import java.util.ArrayList;
+
+public class Pais {
+    private String nombre;
+    public int carrosVendidos;
+    private static ArrayList<Pais> paises = new ArrayList<>();
+
+    public Pais (String nombre){
+        this.nombre = nombre;
+        paises.add(this);
+    }
+    public Pais (){}
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getCarrosVendidos() {
+        return carrosVendidos;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCarrosVendidos(int carrosVendidos) {
+        this.carrosVendidos = carrosVendidos;
+    }
+    public static Pais paisMasVendedor(){
+        int masVendedor = 0;
+        Pais pais = null;
+        for (Pais p: paises){
+            if (p.getCarrosVendidos() > masVendedor){
+                pais = p;
+            }
+        }
+        return pais;
+    }
+}
